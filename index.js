f9c456a6a681c885b2495d2069e618bf162783e5
ACACACanne <annechabab07@gmail.com>
2025-10-17 01:38:34 +0100

    basic design and functions, api set up

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello, World!");
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3002;
+const path = require('path');
+const booksRoutes = require('./routes/booksRoutes');
+
+app.use('/api', booksRoutes);
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Book List!');
+});
+
+
+module.exports = app;
+
+
+
+
